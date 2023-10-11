cbd3f60b512aac00c7eb28b6c26b434a396b3f87
tlop503 <troy.n.lopez@gmail.com>
2023-10-11 13:46:19 -0700

    wget lab2

---
+++ lab2/lab2/Q1.cpp
@@ -0,0 +1,29 @@
+/* CS162- Lab 1 - Q. 1*
+ * Solution description: check the properties, like value and address, of pointer
+ */
+ 
+#include <iostream>
+
+using namespace std;
+
+
+void fooA(int* iptr){
+     /*Print the value pointed to by iptr*/
+     
+     /*Print the address pointed to by iptr*/
+
+     /*Print the address of iptr itself*/
+}
+
+int main(){
+    
+    /*declare an integer x and initialize it with 0*/
+    
+    /*print the address of x*/
+    
+    /*Call fooA() with the address of x*/
+    
+    /*print the value of x*/
+
+    return 0;
+}
---
+++ lab2/lab2/Q2.cpp
@@ -0,0 +1,35 @@
+/* CS 162- Lab 1 - Q.2
+ * Solution description: call the function foo using "reference" to see the values before and after the function
+ */
+ 
+#include <iostream>
+
+using namespace std;
+
+int foo(int* a, int& b, int c){
+    /*Set a to double its original value*/
+
+    /*Set b to half its original value*/
+
+    /*Assign a+b to c*/
+
+    /*Return c*/
+    return 0;
+}
+
+int main(){
+    /*Declare three integers x,y and z and initialize them to 7, 8, 9 respectively*/
+    
+    /*Print the values of x, y and z*/
+
+    /*Call foo() appropriately, passing x,y,z as parameters*/
+
+    /*Print the value returned by foo*/
+
+    /*Print the values of x, y and z again*/
+
+    /*Is the return value different than the value of z?  Why? */
+    return 0;
+}
+    
+    
