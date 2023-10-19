f91d0fff9fe808153ddb28d0ca10bf95bf192149
tlop503 <troy.n.lopez@gmail.com>
2023-10-19 12:12:11 -0700

    lab 3 p1 (monofile)

--- labs/lab3/multdiv.cpp
+++ labs/lab3/multdiv.cpp
@@ -19,31 +19,60 @@ multdiv_entry** create_table(int row, int col){
 	for (int i = 0; i < row; i++) {
 		out[i] = new multdiv_entry[col];
 	}
+	for (int r = 0; r < row; r++) {
+		for (int c = 0; c < col; c++ /*lol*/) {
+			out[r][c].mult = ((r + 1) * (c + 1));
+			out[r][c].div = (float(r + 1) / float(c + 1));
+		}
+	}
 
 	return out;
 }
 
-//iterate and print table
+//iterate, populate, and print table
 void print_table(multdiv_entry** tables, int row, int col) {
+	cout << "mult:" << endl;
 	for (int r = 0; r < row; r++) {
 		for (int c = 0; c < col; c++ /*lol*/) {
-			tables[r][c].mult = (r * c);
-			tables[r][c].div = (r / c);
+			cout << " " << tables[r][c].mult << " ";
 		}
+		cout << endl;
 	}
+	cout << "div:" << endl;
+	for (int r = 0; r < row; r++) {
+		for (int c = 0; c < col; c++ /*lol*/) {
+			cout << " " << tables[r][c].div << " ";
+		}
+		cout << endl;
+	}
+}
+
+void delete_table(multdiv_entry** tables, int row){
+	for (int r = 0; r < row; r++) {
+		delete[] tables[r];
+	}
+	delete[] tables;
 }
 
 int main()
 {
-	//Your code here:
-	int row, col;
-	
-	cout << "row, column" << endl;
-	cin >> row;
-	cin >> col;
+	string in = "no";
+	do {
+		//Your code here:
+		int row, col;
+		
+		cout << "row, column" << endl;
+		cin >> row;
+		cin >> col;
 
-	multdiv_entry** table = create_table(row, col);
-	
-	
+		multdiv_entry** table = create_table(row, col);
+		print_table(table, row, col);
+		cout << "again?" << endl;
+		cin >> in;
+		delete_table(table, row);
+	}
+	while (in == "y");
+
+
 	return 0;
 }
