c2597c2315acb8f10d4a21ceb7fbcb6ee7256d70
tlop503 <troy.n.lopez@gmail.com>
2023-10-20 16:34:13 -0700

    initial work catalog.cpp

--- projs/badsql/assignment2/catalog.cpp
+++ projs/badsql/assignment2/catalog.cpp
@@ -2,4 +2,70 @@
 
 using namespace std;
 
-//function definitions go here
+
+/**************************************************
+ * Name: create_teams()
+ * Description: This function will dynamically allocate
+                an array of teams (of the requested size)
+ * Parameters: int - size of the array
+ * Pre-conditions: none
+ * Post-conditions: a Team array of requested size is created and return
+ ***********************************************/
+Team* create_teams(int);
+
+
+/**************************************************
+ * Name: populate_team_data()
+ * Description: This function will fill a single team struct 
+                with information that is read in from the file
+ * Parameters:  Team* - pointer to the Team array
+                int - index of the Team in the array to be filled 
+                ifstream& - input file to get information from
+ * Pre-conditions: Team array has been allocated; 
+                   provided index is less than the array size
+ * Post-conditions: a Team at provided index is populated
+ ***********************************************/
+void populate_team_data(Team*, int, ifstream &); 
+
+
+/**************************************************
+ * Name: create_players()
+ * Description: This function will dynamically allocate
+                an array of players (of the requested size)
+ * Parameters: int - size of the array
+ * Pre-conditions: none
+ * Post-conditions: a Player array of requested size is created and return
+ ***********************************************/
+Player* create_players(int size) {
+    out = new Player*[size];
+    for (int i = 0; i < size; i++) {
+        populate_player_data
+    }
+}
+
+
+/**************************************************
+ * Name: populate_player_data()
+ * Description: This function will fill a single player struct 
+                with information that is read in from the file
+ * Parameters:  Player* - pointer to the Player array
+                int - index of the Player in the array to be filled 
+                ifstream& - input file to get information from
+ * Pre-conditions: Player array has been allocated; 
+                   provided index is less than the array size
+ * Post-conditions: a Player at provided index is populated
+ ***********************************************/
+void populate_player_data(Player*, int, ifstream &); 
+
+
+/**************************************************
+ * Name: delete_info()
+ * Description: This function will  delete all the memory that was dynamically allocated
+ * Parameters: Team* - the Team array
+ * Pre-conditions: the provided Team array hasn't been freed yet
+ * Post-conditions: the Team struct, with all Players inside, is freed
+ ***********************************************/
+void delete_info(Team* team, int){
+    delete[] team.Player;
+    delete team;
+}
