c93e6ae65c3d7e06ba2dbf658f3b4a2673d4a020
tlop503 <troy.n.lopez@gmail.com>
2023-11-02 12:16:43 -0700

    add makefile

--- projs/coffee/coffee.cpp
+++ projs/coffee/coffee.cpp
@@ -1,6 +1,36 @@
 #include "coffee.h"
 
+
 using namespace std;
+
+//default con, values are 0
+Coffee::Coffee() {
+    name = "";
+    small_cost = 0;
+    medium_cost = 0;
+    large_cost = 0;
+
+}
+
+// Coffee::Coffee(string name) {
+//     this->name = name;
+//     small_cost = 0;
+//     medium_cost = 0;
+//     large_cost = 0;
+//     cout << "Coffee " << this->name << " created" << endl;
+// }
+
+
+//Coffee constructor
+// void Coffee::set_name (const string){}
+// void Coffee::set_small_cost(const float){}
+// void Coffee::set_medium_cost(const float){}
+// void Coffee::set_large_cost(const float){}
+// string Coffee::get_name() const{}
+// float Coffee::get_small_cost () const{}
+// float Coffee::get_medium_cost () const{}
+// float Coffee::get_large_cost () const{}
+// void Coffee::print_coffee() const{} //print the coffee object
 
 
 //function defintions from coffee.h goes here
--- projs/coffee/coffee.h
+++ projs/coffee/coffee.h
@@ -17,6 +17,18 @@ class Coffee {
     //need to include accessor functions and mutator functions for private member when appropriate
     //need to include constructors and destructors where appropriate
     //need to use 'const' when appropriate
+
+    Coffee(); //Coffee constructor
+    Coffee(string name); //alt con
+    void set_name (const string);
+    void set_small_cost(const float);
+    void set_medium_cost(const float);
+    void set_large_cost(const float);
+    string get_name() const;
+    float get_small_cost () const;
+    float get_medium_cost () const;
+    float get_large_cost () const;
+    void print_coffee() const; //print the coffee object
 };
 
 #endif
