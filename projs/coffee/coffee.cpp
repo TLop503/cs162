8e4e78664e0702420a1a69f56a10de92fb8c4ad6
tlop503 <troy.n.lopez@gmail.com>
2023-11-02 12:28:20 -0700

    getters and setters for coffee

--- projs/coffee/coffee.cpp
+++ projs/coffee/coffee.cpp
@@ -1,5 +1,5 @@
 #include "coffee.h"
-
+#include <iostream>
 
 using namespace std;
 
@@ -12,25 +12,49 @@ Coffee::Coffee() {
 
 }
 
-// Coffee::Coffee(string name) {
-//     this->name = name;
-//     small_cost = 0;
-//     medium_cost = 0;
-//     large_cost = 0;
-//     cout << "Coffee " << this->name << " created" << endl;
-// }
+Coffee::Coffee(string name) {
+    this->name = name;
+    small_cost = 0;
+    medium_cost = 0;
+    large_cost = 0;
+    cout << "Coffee " << this->name << " created" << endl;
+}
 
 
 //Coffee constructor
-// void Coffee::set_name (const string){}
-// void Coffee::set_small_cost(const float){}
-// void Coffee::set_medium_cost(const float){}
-// void Coffee::set_large_cost(const float){}
-// string Coffee::get_name() const{}
-// float Coffee::get_small_cost () const{}
-// float Coffee::get_medium_cost () const{}
-// float Coffee::get_large_cost () const{}
-// void Coffee::print_coffee() const{} //print the coffee object
+void Coffee::set_name (const string name){
+    this->name = name;
+}
 
+//setters
+void Coffee::set_small_cost(const float cost){
+    small_cost = cost;
+}
+void Coffee::set_medium_cost(const float cost){
+    medium_cost = cost;
+}
+void Coffee::set_large_cost(const float cost){
+    large_cost = cost;
+}
 
-//function defintions from coffee.h goes here
+//getters
+string Coffee::get_name() const{
+    return name;
+}
+float Coffee::get_small_cost () const{
+    return small_cost;
+}
+float Coffee::get_medium_cost () const{
+    return medium_cost;
+}
+float Coffee::get_large_cost () const{
+    return large_cost;
+}
+
+//print
+void Coffee::print_coffee() const{
+    cout << "Coffee name: " << name << endl;
+    cout << "Small Cost: $" << small_cost << endl;
+    cout << "Medium Cost: $" << medium_cost << endl;
+    cout << "Large Cost: $" << large_cost << endl;
+} //print the coffee object
