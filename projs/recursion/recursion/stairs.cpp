f5c5f3adcfe7667b6b7f8cdf2a16fb9aae7fd480
tlop503 <troy.n.lopez@gmail.com>
2023-11-29 19:03:46 -0800

    recursion

--- projs/recursion/recursion/stairs.cpp
+++ projs/recursion/recursion/stairs.cpp
@@ -1,3 +1,29 @@
+/**************************************************
+ * Name: ways_to_top(int n)
+ * Description: sums possible ways to combine 1, 2, 3
+ * in unique orders to reach n
+ * Parameters: n - num to reach
+ * Pre-conditions: n is reasonably (sub 37) int
+ * Post-conditions: will recursively call itself and total everything up
+ ***********************************************/
 int ways_to_top(int n) {
-	// YOUR CODE GOES HERE
+	if (n == 3) {
+		return 4;
+	}
+	if (n == 2) {
+		return 2;
+	}
+	if (n == 1) {
+		return 1;
+	}
+	if (n <= 0) {
+		return 0;
+	}
+	else {
+		//take out 1 block of each size and count rests
+		//kinda makes a tree
+		return (ways_to_top(n-1) +
+				ways_to_top(n-2) +
+				ways_to_top(n-3));
+	}
 }
