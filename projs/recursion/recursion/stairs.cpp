ef3c2d59468ddd25d43f847e1018131758bfbaaf
tlop503 <troy.n.lopez@gmail.com>
2023-12-09 12:31:57 -0800

    stairs comments

--- projs/recursion/recursion/stairs.cpp
+++ projs/recursion/recursion/stairs.cpp
@@ -8,22 +8,22 @@
  ***********************************************/
 int ways_to_top(int n) {
 	if (n == 3) {
-		return 4;
+		return 4; //1+1+1, 1+2, 2+1, 3
 	}
 	if (n == 2) {
-		return 2;
+		return 2; //1+1, 2
 	}
 	if (n == 1) {
-		return 1;
+		return 1; //1
 	}
 	if (n <= 0) {
-		return 0;
+		return 0; //0
 	}
 	else {
 		//take out 1 block of each size and count rests
 		//kinda makes a tree
-		return (ways_to_top(n-1) +
-				ways_to_top(n-2) +
-				ways_to_top(n-3));
+		return (ways_to_top(n-1) + //take out 1 block of size 1
+				ways_to_top(n-2) + //take out 1 block of size 2
+				ways_to_top(n-3)); //take out 1 block of size 3
 	}
 }
